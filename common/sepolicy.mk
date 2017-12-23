9f709be4de5034fbc1e8a2c7c9789453757bd0c8
Demon000 <demonsingur@gmail.com>
2017-12-23 15:07:01 +0200

    sepolicy: common: add rules for Carbon vibrator hal
    Change-Id: I1e8aba0e0ce1a51cfcd54c734d58f10f0613f773

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -8,3 +8,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/carbon/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/carbon/sepolicy/common/vendor
