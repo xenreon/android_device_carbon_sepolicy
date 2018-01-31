LineageOS/android_device_lineage_sepolicy

sepolicy: Include platform specific sepolicy
Change-Id: Ib2c58c95f934477e782fec64d2aa4dc951d69b9d

qcom/sepolicy.mk
@@ -4,4 +4,5 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom
+    device/lineage/sepolicy/qcom/common \
+    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)

xenreon/android_device_carbon_sepolicy

qcom/sepolicy.mk
@@ -4,4 +4,5 @@
 #
 
 BOARD_SEPOLICY_DIRS += \
-    device/aosip/sepolicy/qcom
+    device/aosip/sepolicy/qcom/common \
+    device/aosip/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)